bd1f08719d6f295a7d13768399dcb84a3b3972c2
Bennett Joerger <bennettjoerger@gmail.com>
2018-02-19 14:49:42 -0700

    Updated API

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+var weatherApp = angular.module('weatherApp', ['$sceDelegateProvider','ngRoute', 'ngResource'])
+.config(function  ($sceDelegateProvider,$routeProvider){
 
-weatherApp.config(function($routeProvider) {
+  $sceDelegateProvider.resourceUrlWhitelist([
+  'self',
+  'http://api.openweathermap.org']);
+
 
   $routeProvider
 
@@ -15,6 +19,7 @@ weatherApp.config(function($routeProvider) {
   })
 });
 
+
 weatherApp.service('cityService', function(){
 
   this.city = "New York, NY";
@@ -28,11 +33,18 @@ weatherApp.controller('mainController', ['$scope', 'cityService', function($scop
     cityService.city = $scope.city;
   });
 
-
 }]);
 
-weatherApp.controller('secondController', ['$scope','cityService', function($scope, cityService){
+weatherApp.controller('secondController', ['$scope', '$resource', 'cityService', function($scope, $resource, cityService){
 
   $scope.city = cityService.city;
+
+  $scope.weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast?q={{$scope.city}},uk&appid=7dd407eddaec652ef552be05f83248ab', {callback: "JSON_CALLBACK"}, { get: { method: "JSONP" }});
+
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: 2 });
+
+  console.log(console.weatherAPI);
+
+
 
 }]);
